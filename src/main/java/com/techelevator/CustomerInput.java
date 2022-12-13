2c931b02ef85bf8f5f5148363fdf0e8e3c63f5b6
Aria <ariaodeli21@gmail.com>
2022-12-12 19:30:24 -0600

    final merge fixes

--- src/main/java/com/techelevator/CustomerInput.java
+++ src/main/java/com/techelevator/CustomerInput.java
@@ -1,19 +1,15 @@
 package com.techelevator;
+import java.util.Scanner;
 
-<<<<<<< HEAD
 public class CustomerInput extends Snacks{
     public CustomerInput(String locationOfSnacks, String productName, double price, String snackType) {
         super(locationOfSnacks, productName, price, snackType);
-=======
-import java.util.Scanner;
-
-public class CustomerInput {
-
-    public static void main(String[] args) { // change main and not static
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("Please enter number");
-
-        String customerSelection = userInput.nextLine();
->>>>>>> fd7ae6141fd162ec4e6fe91107d9850fec874693
     }
-}
+        public void scan(){ // change main and not static
+                Scanner userInput = new Scanner(System.in);
+                System.out.println("Please enter number");
+
+                String customerSelection = userInput.nextLine();
+            }
+        }
+    
