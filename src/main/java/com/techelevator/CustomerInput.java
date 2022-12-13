16e2c9392d7e6ed4dc49c1ad6b2e4b908710b25e
Kendra <kendra.tamara.porter.com>
2022-12-12 17:36:22 -0800

    Small change

--- src/main/java/com/techelevator/CustomerInput.java
+++ src/main/java/com/techelevator/CustomerInput.java
@@ -5,11 +5,10 @@ public class CustomerInput extends Snacks{
     public CustomerInput(String locationOfSnacks, String productName, double price, String snackType) {
         super(locationOfSnacks, productName, price, snackType);
     }
-        public void scan(){ // change main and not static
+        public void scan(){ 
                 Scanner userInput = new Scanner(System.in);
                 System.out.println("Please enter number");
 
                 String customerSelection = userInput.nextLine();
             }
         }
-    
