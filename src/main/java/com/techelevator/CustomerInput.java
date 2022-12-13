1ecbb57d2800d9db1c6941b2cfa57876b79553f6
Aria <ariaodeli21@gmail.com>
2022-12-12 19:18:25 -0600

    fixing merge issue

--- src/main/java/com/techelevator/CustomerInput.java
+++ src/main/java/com/techelevator/CustomerInput.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class CustomerInput {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // change main and not static
         Scanner userInput = new Scanner(System.in);
         System.out.println("Please enter number");
 
